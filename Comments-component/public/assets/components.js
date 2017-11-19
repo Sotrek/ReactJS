c0af531b52a6d3715dc57beedc14200277ddba84
Sotrek <sotrek@gmail.com>
2017-11-19 11:58:03 +0000

    adding Avatar Url and commentCount method

--- Comments-component/public/assets/components.js
+++ Comments-component/public/assets/components.js
@@ -2,6 +2,7 @@ class Comment extends React.Component {
   render() {
     return(
       <div className="comment">
+        <img className="comment" src={this.props.avatarUrl} alt={`${this.props.author}'s picture`}/>
         <p className="comment-header">
           {this.props.author}
         </p>
@@ -22,6 +23,7 @@ class CommentBox extends React.Component {
     return(
       <div className="comment-box">
         <h3>Comments</h3>
+         {this._getPopularMessage(comments.length)}
         <h4 className="comment-count">{this._getCommentsTitle(comments.length)}</h4>
         <div className="comment-list">
             {comments}
@@ -31,8 +33,8 @@ class CommentBox extends React.Component {
   }
   _getComment() {
     const commentList = [
-      { id: 1, author: 'Clu', body: 'Just say no to love!', avatarUrl: 'images/default-avatar.png' },
-      { id: 2, author: 'Anne Droid', body: 'I wanna know what love is...', avatarUrl: 'images/default-avatar.png' }
+      { id: 1, author: 'Clu', body: 'Just say no to love!', avatarUrl: 'assets/images/avatars/avatar-default.png' },
+      { id: 2, author: 'Anne Droid', body: 'I wanna know what love is...', avatarUrl: 'assets/images/avatars/avatar-default.png' }
     ];    
     return commentList.map((comment)=> <Comment author={comment.author} body={comment.body} avatarUrl={comment.avatarUrl} key={comment.id}/>)
   }
@@ -44,6 +46,16 @@ class CommentBox extends React.Component {
       } else {
         return `${commentsCount} comments`;
       }
+  }
+  _getPopularMessage(commentCount) {
+    const POPULAR_COUNT = 10;
+    
+    if(commentCount > POPULAR_COUNT){
+      return(<div>
+              This post is getting really popular, don't miss out!
+        </div>);
+    }
+    
   }
 }
 
